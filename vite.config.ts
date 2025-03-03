bc40b41f9fab7da37cedc4c1fd606ee55e84a9c5
ELAMLI <158856708+elamli9@users.noreply.github.com>
2025-03-03 22:23:14 +0000

    Fixed base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'panel/',
+  base: '/ELAMLI_PAD/', // ✅ Add a leading slash
   optimizeDeps: {
     exclude: ['lucide-react'],
-    
   },
 });
